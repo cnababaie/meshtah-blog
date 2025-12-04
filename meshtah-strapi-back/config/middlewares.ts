5e52c6ac60765d47277624508cccd00c30fc6a0d
Sina Babaie <cnababaie@gmail.com>
2025-12-04 22:57:10 +0330

    Server

--- meshtah-strapi-back/config/middlewares.ts
+++ meshtah-strapi-back/config/middlewares.ts
@@ -1,4 +1,20 @@
 export default [
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      cookie: {
+        'strapi-admin': {
+          httpOnly: true,
+          secure: true,
+          sameSite: 'none',
+          domain: 'meshtah.net',
+          path: '/',
+        },
+      },
+    },
+  }
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
